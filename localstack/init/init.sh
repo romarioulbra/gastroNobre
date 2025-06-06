ecdd281071ce50feae3d288ca782b744485d352b
romarioulbra <114452042+romarioulbra@users.noreply.github.com>
2025-06-06 16:56:26 -0300

    ajus

--- localstack/init/init.sh
+++ localstack/init/init.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 # Criar a tabela DynamoDB "pedidos"
-# awslocal dynamodb create-table \
-#   --table-name pedidos \
-#   --attribute-definitions AttributeName=id,AttributeType=S \
-#   --key-schema AttributeName=id,KeyType=HASH \
-#   --billing-mode PAY_PER_REQUEST
-
-# # Criar fila SQS "pedidos-cozinha"
-# awslocal sqs create-queue --queue-name pedidos-cozinha
-
-# # Criar bucket S3 "comprovantes-pedidos"
-# awslocal s3api create-bucket --bucket comprovantes-pedidos
-
-# # Criar notificação "PedidosConcluidos"
-# echo "Criando tópico SNS..."
-# awslocal sns create-topic --name PedidosConcluidos
-
-#!/bin/bash
-
-# Criar a tabela DynamoDB "pedidos"
 awslocal dynamodb create-table \
   --table-name pedidos \
   --attribute-definitions AttributeName=id,AttributeType=S \
